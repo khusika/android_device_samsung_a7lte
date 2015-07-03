3edeade227eb827cb86ef1b61ee5fe6e175b8a12
Jackeagle <jackeagle102@gmail.com>
2015-07-03 01:42:54 -0400

    a7lte : Add Kernel Source To Build
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,11 +14,10 @@
 # limitations under the License.
 #
 
-USE_CAMERA_STUB := true
-
-# inherit from the proprietary version
+# Inherit from the proprietary version
 -include vendor/samsung/a7lte/BoardConfigVendor.mk
 
+# Architecture
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := unknown
@@ -29,12 +28,19 @@ TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-TARGET_BOOTLOADER_BOARD_NAME := a7lte
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8916
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci
 BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
+TARGET_KERNEL_CONFIG := msm8939_sec_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8939_sec_a7_eur_defconfig
+TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/a7lte
 
 # Partition Info
 BOARD_BOOTIMAGE_PARTITION_SIZE := 13631488
